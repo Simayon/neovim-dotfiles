8fa15c256fe9ef980fc64c9d6c396063cdce8ea7
Simayon Thampi <simayonthampi@gmail.com>
2024-08-25 18:35:15 +0530

    fix: Edited Surround Nvim Plugin
    [Description]:
    + Edited surround plugin to list more details on the keybindings
      available on nvim
    
    [Notes]:

--- init.lua
+++ init.lua
@@ -607,13 +607,32 @@ require('lazy').setup({
       --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
       --  - ci'  - [C]hange [I]nside [']quote
       require('mini.ai').setup { n_lines = 500 }
-
+      -- Add/delete/replace surroundings (brackets, quotes, etc.)
+      require('mini.surround').setup {
+        -- No need to copy this inside `setup()`. Will be used automatically.
+        custom_surroundings = nil,
+        highlight_duration = 500,
+        mappings = {
+          add = 'sa', -- Add surrounding in Normal and Visual modes
+          delete = 'sd', -- Delete surrounding
+          find = 'sf', -- Find surrounding (to the right)
+          find_left = 'sF', -- Find surrounding (to the left)
+          highlight = 'sh', -- Highlight surrounding
+          replace = 'sr', -- Replace surrounding
+          update_n_lines = 'sn', -- Update `n_lines`
+          suffix_last = 'l', -- Suffix to search with "prev" method
+          suffix_next = 'n', -- Suffix to search with "next" method
+        },
+        n_lines = 20,
+        respect_selection_type = false,
+        search_method = 'cover',
+        silent = false,
+      }
       -- Add/delete/replace surroundings (brackets, quotes, etc.)
       --
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
