cab2dcaf01a742424b09b131abeb1106c90be88d
Simayon Thampi <simayonthampi@gmail.com>
2024-12-24 15:16:36 +0530

    fix(copilot): Fix Copilot Completion
    1. Fixed a bug causing copilot completion to fail

--- lua/plugins/lsp/completion.lua
+++ lua/plugins/lsp/completion.lua
@@ -13,6 +13,9 @@ return {
 				require("luasnip").setup({})
 			end,
 		},
+		{
+			"giuxtaposition/blink-cmp-copilot",
+		},
 	},
 	build = "cargo build --release",
 	opts = {
@@ -39,16 +42,52 @@ return {
 				},
 				copilot = {
 					name = "copilot",
-					enabled = true,
 					module = "blink-cmp-copilot",
-					score_offset = -100,
+					score_offset = 100,
 					async = true,
+					transform_items = function(_, items)
+						local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+						local kind_idx = #CompletionItemKind + 1
+						CompletionItemKind[kind_idx] = "Copilot"
+						for _, item in ipairs(items) do
+							item.kind = kind_idx
+						end
+						return items
+					end,
 				},
 			},
 		},
 		appearance = {
 			use_nvim_cmp_as_default = false,
 			nerd_font_variant = "mono",
+			kind_icons = {
+				Copilot = "",
+				Text = '󰉿',
+				Method = '󰊕',
+				Function = '󰊕',
+				Constructor = '󰒓',
+				Field = '󰜢',
+				Variable = '󰆦',
+				Property = '󰖷',
+				Class = '󱡠',
+				Interface = '󱡠',
+				Struct = '󱡠',
+				Module = '󰅩',
+				Unit = '󰪚',
+				Value = '󰦨',
+				Enum = '󰦨',
+				EnumMember = '󰦨',
+				Keyword = '󰻾',
+				Constant = '󰏿',
+				Snippet = '󱄽',
+				Color = '󰏘',
+				File = '󰈔',
+				Reference = '󰬲',
+				Folder = '󰉋',
+				Event = '󱐋',
+				Operator = '󰪚',
+				TypeParameter = '󰬛',
+			},
 		},
 		completion = {
 			menu = {
---
+++ lua/plugins/lsp/copilot.lua
@@ -0,0 +1,12 @@
+return {
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	build = ":Copilot auth",
+	event = "InsertEnter",
+	config = function()
+		require("copilot").setup({
+			suggestion = { enabled = false },
+			panel = { enabled = false },
+		})
+	end,
+}
