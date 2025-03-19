de068dcbdf4bc7b78ec8afc1612635e021bf3b8e
Simayon Thampi <simayonthampi@gmail.com>
2025-03-19 15:50:03 +0530

    fix(completion): Update Completion Config

--- lua/plugins/lsp/completion.lua
+++ lua/plugins/lsp/completion.lua
@@ -1,186 +1,279 @@
 return {
-	"Saghen/blink.cmp",
-	event = "InsertEnter",
-	build = "cargo build --release",
-	dependencies = {
-		{
-			"rafamadriz/friendly-snippets",
-			config = function()
-				require("luasnip.loaders.from_vscode").lazy_load()
-			end,
-		},
-		"jc-doyle/cmp-pandoc-references",
-		{
+	{
+		"saghen/blink.cmp",
+		dependencies = {
 			"L3MON4D3/LuaSnip",
-			dependencies = {
-				{
-					"iurimateus/luasnip-latex-snippets.nvim",
-					ft = { "tex", "bib" },
-				},
+			"mikavilpas/blink-ripgrep.nvim",
+			"Kaiser-Yang/blink-cmp-dictionary",
+			{
+				"Kaiser-Yang/blink-cmp-git",
+				dependencies = { "nvim-lua/plenary.nvim" },
 			},
-			config = function()
-				local ls = require("luasnip")
-				local ls_types = require("luasnip.util.types")
+		},
+		version = "*",
 
-				ls.setup({
-					keep_roots = true,
-					link_roots = true,
-					exit_roots = false,
-					link_children = true,
-					region_check_events = "CursorMoved,CursorMovedI",
-					delete_check_events = "TextChanged,TextChangedI",
-					enable_autosnippets = true,
-					store_selection_keys = "<Tab>",
-					ext_opts = {
-						[ls_types.insertNode] = {
-							unvisited = {
-								virt_text_pos = "inline",
-							},
-						},
-						[ls_types.exitNode] = {
-							unvisited = {
-								virt_text_pos = "inline",
-							},
-						},
+		---@module 'blink.cmp'
+		---@type blink.cmp.Config
+		opts = {
+			fuzzy = {
+				use_frecency = false,
+			},
+			completion = {
+				accept = {
+					auto_brackets = {
+						enabled = false,
 					},
-				})
-
-				-- Unlink current snippet on leaving insert/selection mode
-				-- https://github.com/L3MON4D3/LuaSnip/issues/258#issuecomment-1011938524
-				vim.api.nvim_create_autocmd("ModeChanged", {
-					desc = "Unlink current snippet on leaving insert/selection mode.",
-					group = vim.api.nvim_create_augroup("LuaSnipModeChanged", {}),
-					callback = function(info)
-						local mode = vim.v.event.new_mode
-						local omode = vim.v.event.old_mode
-						if
-							(omode == "s" and mode == "n" or omode == "i")
-							and ls.session.current_nodes[info.buf]
-							and not ls.session.jump_active
-						then
-							ls.unlink_current()
-						end
-					end,
-				})
-
-				---Load snippets for a given filetype
-				---@param ft string
-				---@return nil
-				local function load_snippets(ft)
-					local ok, snip_groups = pcall(require, "snippets." .. ft)
-					if ok and type(snip_groups) == "table" then
-						for _, snip_group in pairs(snip_groups) do
-							ls.add_snippets(ft, snip_group.snip or snip_group, snip_group.opts or {})
-						end
-					end
-				end
-
-				-- Lazy-load snippets based on filetype
-				for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-					load_snippets(vim.bo[buf].ft)
-				end
-				vim.api.nvim_create_autocmd("FileType", {
-					group = vim.api.nvim_create_augroup("LuaSnipLazyLoadSnippets", {}),
-					desc = "Lazy load snippets for different filetypes.",
-					callback = function(info)
-						load_snippets(vim.bo[info.buf].ft)
-					end,
-				})
-			end,
-		},
-		{ "saghen/blink.compat", version = "*", opts = { impersonate_nvim_cmp = true } },
-		"mikavilpas/blink-ripgrep.nvim",
-	},
-	---@module 'blink.cmp'
-	---@type blink.cmp.Config
-	opts = {
-		keymap = { preset = "super-tab" },
-		appearance = {
-			use_nvim_cmp_as_default = true,
-			nerd_font_variant = "mono",
-		},
-		snippets = {
-			expand = function(snippet)
-				require("luasnip").lsp_expand(snippet)
-			end,
-			active = function(filter)
-				if filter and filter.direction then
-					return require("luasnip").jumpable(filter.direction)
-				end
-				return require("luasnip").in_snippet()
-			end,
-			jump = function(direction)
-				require("luasnip").jump(direction)
-			end,
-		},
-		completion = {
-			documentation = { auto_show = false },
-			ghost_text = { enabled = false },
-			menu = {
-				draw = {
-					columns = {
-						{ "kind_icon" },
-						{ "label", "label_description", gap = 1, "kind" },
-					},
-					components = {
-						kind_icon = {
-							text = function(ctx)
-								return " " .. ctx.kind_icon .. ctx.icon_gap .. " "
-							end,
+				},
+				list = {
+					selection = { preselect = false, auto_insert = true },
+					max_items = 20,
+				},
+				menu = {
+					border = "rounded",
+					max_height = 15,
+					scrolloff = 0,
+					draw = {
+						align_to = "cursor",
+						padding = 0,
+						columns = {
+							{ "kind_icon" },
+							{ "label", "label_description", gap = 1 },
+							{ "source_name" },
 						},
-						kind = {
-							text = function(ctx)
-								return "(" .. ctx.kind .. ")"
-							end,
-							highlight = function(ctx)
-								return require("blink.cmp.completion.windows.render.tailwind").get_hl(ctx)
-									or "BlinkCmpCustomType"
-							end,
+						components = {
+							source_name = {
+								text = function(ctx)
+									return "[" .. ctx.source_name .. "]"
+								end,
+							},
+							label = {
+								text = function(ctx)
+									if not vim.g.rime_enabled then
+										return ctx.label .. ctx.label_detail
+									end
+									local client = vim.lsp.get_client_by_id(ctx.item.client_id)
+									if not client or client.name ~= "rime_ls" then
+										return ctx.label .. ctx.label_detail
+									end
+									local code_start = #ctx.label_detail + 1
+									for i = 1, #ctx.label_detail do
+										local ch = string.sub(ctx.label_detail, i, i)
+										if ch >= "a" and ch <= "z" then
+											code_start = i
+											break
+										end
+									end
+									local code_end = #ctx.label_detail - 4
+									return ctx.label
+										.. " <"
+										.. string.gsub(
+											string.sub(ctx.label_detail, code_start, code_end),
+											"  ·  ",
+											" "
+										)
+										.. ">"
+								end,
+							},
 						},
 					},
 				},
+				documentation = {
+					auto_show = true,
+					auto_show_delay_ms = 0,
+					window = {
+						border = "rounded",
+					},
+				},
 			},
-		},
-		sources = {
-			default = { "lsp", "path", "snippets", "buffer", "luasnip", "ripgrep", "supermaven" },
-			per_filetype = {
-				markdown = {
+			signature = {
+				enabled = true,
+				window = {
+					border = "rounded",
+				},
+			},
+			keymap = {
+				preset = "none",
+				["<cr>"] = { "accept", "fallback" },
+				["<tab>"] = { "snippet_forward", "fallback" },
+				["<s-tab>"] = { "snippet_backward", "fallback" },
+				["<c-u>"] = { "scroll_documentation_up", "fallback" },
+				["<c-d>"] = { "scroll_documentation_down", "fallback" },
+				["<c-j>"] = { "select_next", "fallback" },
+				["<c-k>"] = { "select_prev", "fallback" },
+				["<c-x>"] = { "show", "fallback" },
+				["<c-c>"] = { "cancel", "fallback" },
+				["<space>"] = {
+					function(cmp)
+						if not vim.g.rime_enabled then
+							return false
+						end
+						local rime_item_index = require("plugins.rime_ls").get_n_rime_item_index(1)
+						if #rime_item_index ~= 1 then
+							return false
+						end
+						return cmp.accept({ index = rime_item_index[1] })
+					end,
+					"fallback",
+				},
+				[";"] = {
+					-- FIX: can not work when binding ;<space> to other key
+					function(cmp)
+						if not vim.g.rime_enabled then
+							return false
+						end
+						local rime_item_index = require("plugins.rime_ls").get_n_rime_item_index(2)
+						if #rime_item_index ~= 2 then
+							return false
+						end
+						return cmp.accept({ index = rime_item_index[2] })
+					end,
+					"fallback",
+				},
+				["z"] = {
+					function(cmp)
+						if not vim.g.rime_enabled then
+							return false
+						end
+						local content_before_cursor =
+							string.sub(vim.api.nvim_get_current_line(), 1, vim.api.nvim_win_get_cursor(0)[2])
+						if content_before_cursor:match("z%w*$") then
+							return false
+						end
+						local rime_item_index = require("plugins.rime_ls").get_n_rime_item_index(3)
+						if #rime_item_index ~= 3 then
+							return false
+						end
+						return cmp.accept({ index = rime_item_index[3] })
+					end,
+					"fallback",
+				},
+			},
+			appearance = {
+				nerd_font_variant = "mono",
+			},
+			snippets = { preset = "luasnip" },
+			sources = {
+				default = {
 					"lsp",
 					"path",
 					"snippets",
 					"buffer",
-					"luasnip",
 					"ripgrep",
-					"supermaven",
-					"pandoc_references",
+					"lazydev",
+					"dictionary",
+					"git",
+					"markdown",
 				},
-				tex = { "lsp", "path", "snippets", "buffer", "luasnip", "ripgrep", "supermaven", "pandoc_references" },
-			},
-			providers = {
-				ripgrep = {
-					module = "blink-ripgrep",
-					name = "Ripgrep",
-					score_offset = -4,
-					opts = {
-						prefix_min_len = 3,
-						context_size = 5,
-						max_filesize = "1G",
+				providers = {
+					markdown = {
+						name = "Render",
+						module = "render-markdown.integ.blink",
+						fallbacks = { "lsp" },
 					},
-				},
-				pandoc_references = {
-					name = "pandoc_references",
-					module = "blink.compat.source",
-					score_offset = -1,
-				},
-				supermaven = {
-					name = "supermaven",
-					module = "blink.compat.source",
-					score_offset = 100,
-					async = true,
+					git = {
+						-- Because we use filetype to enable the source,
+						-- we can make the score higher
+						score_offset = 100,
+						module = "blink-cmp-git",
+						name = "Git",
+						-- enabled this source at the beginning to make it possible to pre-cache
+						-- at very beginning
+						enabled = true,
+						-- only show this source when filetype is gitcommit or markdown
+						should_show_items = function()
+							return vim.o.filetype == "gitcommit" or vim.o.filetype == "markdown"
+						end,
+						--- @module 'blink-cmp-git'
+						--- @type blink-cmp-git.Options
+						opts = {},
+					},
+					dictionary = {
+						module = "blink-cmp-dictionary",
+						name = "Dict",
+						min_keyword_length = 3,
+						max_items = 5,
+						score_offset = -3,
+						--- @module 'blink-cmp-dictionary'
+						--- @type blink-cmp-dictionary.Options
+						opts = {
+							dictionary_files = { vim.fn.expand("~/.config/nvim/dict/en_dict.txt") },
+						},
+					},
+					lsp = {
+						fallbacks = nil,
+						--- @param context blink.cmp.Context
+						--- @param items blink.cmp.CompletionItem[]
+						transform_items = function(context, items)
+							local TYPE_ALIAS = require("blink.cmp.types").CompletionItemKind
+							-- demote snippets
+							for _, item in ipairs(items) do
+								if item.kind == TYPE_ALIAS.Snippet then
+									item.score_offset = item.score_offset - 3
+								end
+							end
+							-- filter non-acceptable rime items
+							--- @param item blink.cmp.CompletionItem
+							return vim.tbl_filter(function(item)
+								local rime_ls = require("plugins.rime_ls")
+								if not rime_ls.is_rime_item(item) and item.kind ~= TYPE_ALIAS.Text then
+									return true
+								end
+								if require("utils").rime_ls_disabled(context) then
+									return false
+								end
+								item.detail = nil
+								return rime_ls.rime_item_acceptable(item)
+							end, items)
+						end,
+					},
+					buffer = { max_items = 5 },
+					snippets = { name = "Snip" },
+					lazydev = {
+						name = "LazyDev",
+						module = "lazydev.integrations.blink",
+						score_offset = 100,
+					},
+					ripgrep = {
+						module = "blink-ripgrep",
+						name = "RG",
+						max_items = 5,
+						---@module 'blink-ripgrep'
+						---@type blink-ripgrep.Options
+						opts = {
+							prefix_min_len = 3,
+							context_size = 5,
+							max_filesize = "1M",
+							project_root_marker = vim.g.root_markers,
+							search_casing = "--smart-case",
+							project_root_fallback = false,
+							-- (advanced) Any additional options you want to give to ripgrep.
+							-- See `rg -h` for a list of all available options. Might be
+							-- helpful in adjusting performance in specific situations.
+							-- If you have an idea for a default, please open an issue!
+							--
+							-- Not everything will work (obviously).
+							additional_rg_options = {},
+							-- When a result is found for a file whose filetype does not have a
+							-- treesitter parser installed, fall back to regex based highlighting
+							-- that is bundled in Neovim.
+							fallback_to_regex_highlighting = true,
+						},
+					},
+					path = {
+						fallbacks = { "buffer", "ripgrep" },
+						opts = {
+							trailing_slash = false,
+							show_hidden_files_by_default = true,
+						},
+					},
 				},
 			},
 		},
-		signature = { enabled = true },
+		opts_extend = { "sources.default" },
+		-- TODO: add those below:
+		-- { name = 'nvim_lua' },
+		-- { name = "calc",   max_item_count = 3 },
+		-- { name = "vimtex", },
+		-- { name = "cmp_yanky", max_item_count = 3 },
 	},
-	opts_extend = { "sources.default" },
 }
