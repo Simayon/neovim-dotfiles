676bb5396df7ba309d9d395602073b5623268c69
Simayon Thampi <simayonthampi@gmail.com>
2025-10-01 15:49:08 +0530

    refactor: Change Treesitter Configuration

--- lua/configs/performance.lua
+++ lua/configs/performance.lua
@@ -2,61 +2,66 @@
 local M = {}
 
 function M.setup()
-    -- Reduce the time that neovim waits after each keystroke
-    vim.opt.updatetime = 250
-    vim.opt.timeout = true
-    vim.opt.timeoutlen = 300
+	-- Reduce the time that neovim waits after each keystroke
+	vim.opt.updatetime = 250
+	vim.opt.timeout = true
+	vim.opt.timeoutlen = 300
 
-    -- Disable some built-in plugins we don't use
-    local disabled_built_ins = {
-        "gzip",
-        "zip",
-        "zipPlugin",
-        "tar",
-        "tarPlugin",
-        "getscript",
-        "getscriptPlugin",
-        "vimball",
-        "vimballPlugin",
-        "2html_plugin",
-        "logipat",
-        "rrhelper",
-        "spellfile_plugin",
-        "matchit"
-    }
+	-- Disable some built-in plugins we don't use
+	local disabled_built_ins = {
+		"gzip",
+		"zip",
+		"zipPlugin",
+		"tar",
+		"tarPlugin",
+		"getscript",
+		"getscriptPlugin",
+		"vimball",
+		"vimballPlugin",
+		"2html_plugin",
+		"logipat",
+		"rrhelper",
+		"spellfile_plugin",
+		"matchit",
+	}
 
-    for _, plugin in pairs(disabled_built_ins) do
-        vim.g["loaded_" .. plugin] = 1
-    end
+	for _, plugin in pairs(disabled_built_ins) do
+		vim.g["loaded_" .. plugin] = 1
+	end
 
-    -- More efficient lua module loading
-    vim.loader.enable()
+	-- More efficient lua module loading
+	vim.loader.enable()
 
-    -- Configure LSP to be less aggressive
-    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
-        vim.lsp.handlers.hover, {
-            -- Reduce update time for hover
-            update_events = {"CursorHold", "CursorHoldI"},
-        }
-    )
+	-- Configure LSP hover to be less aggressive
+	vim.lsp.handlers["textDocument/hover"] = vim.lsp.buf.hover({
+		-- Reduce resource usage for hover windows
+		border = "rounded",
+		max_width = 80,
+		max_height = 20,
+	})
 
-    -- Configure treesitter to be less resource intensive if it's available
-    local ok, treesitter = pcall(require, 'nvim-treesitter.configs')
-    if ok then
-        treesitter.setup {
-            highlight = {
-                enable = true,
-                -- Disable slow treesitter highlight for large files
-                disable = function(lang, buf)
-                    local max_filesize = 100 * 1024 -- 100 KB
-                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                    if ok and stats and stats.size > max_filesize then
-                        return true
-                    end
-                end,
-            },
-        }
-    end
+	-- Configure treesitter to be less resource intensive if it's available
+	local ok, treesitter = pcall(require, "nvim-treesitter.configs")
+	if ok then
+		treesitter.setup({
+			modules = {},
+			sync_install = false,
+			ensure_installed = {}, -- can be set to "all" or list of languages
+			ignore_install = {},
+			auto_install = false,
+			highlight = {
+				enable = true,
+				-- Disable treesitter for large files (>100KB)
+				disable = function(_, buf)
+					local max_filesize = 100 * 1024 -- 100 KB
+					local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
+					if ok and stats and stats.size > max_filesize then
+						return true
+					end
+				end,
+			},
+		})
+	end
 end
 
 return M
--- lua/plugins/editor/treesitter.lua
+++ lua/plugins/editor/treesitter.lua
@@ -1,95 +1,129 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        event = { "BufReadPost", "BufNewFile" },
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter-textobjects",
-        },
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                -- Automatically install missing parsers when entering buffer
-                auto_install = true,
+	{
+		-- Treesitter: Advanced syntax highlighting and code understanding
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		event = { "BufReadPost", "BufNewFile" },
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+		},
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				-- Required fields for treesitter configuration
+				modules = {},
+				sync_install = true,
+				ignore_install = {},
 
-                -- List of parsers to install
-                ensure_installed = {
-                    "bash", "c", "cpp", "go", "lua", "python", "rust",
-                    "vim", "vimdoc",
-                },
-                
-                -- Enable syntax highlighting
-                highlight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = false,
-                },
+				-- Automatically install missing parsers when entering buffer
+				auto_install = true,
 
-                -- Enable incremental selection
-                incremental_selection = {
-                    enable = true,
-                    keymaps = {
-                        init_selection = "<C-space>",
-                        node_incremental = "<C-space>",
-                        scope_incremental = false,
-                        node_decremental = "<bs>",
-                    },
-                },
+				-- List of language parsers to install
+				ensure_installed = {
+					"bash",
+					"c",
+					"cpp",
+					"go",
+					"lua",
+					"python",
+					"rust",
+					"vim",
+					"vimdoc",
+				},
 
-                -- Enable text objects and movement
-                textobjects = {
-                    -- Define text object selection
-                    select = {
-                        enable = true,
-                        lookahead = true,
-                        keymaps = {
-                            -- You can use the capture groups defined in textobjects.scm
-                            ["af"] = "@function.outer",  -- Select outer part of a function
-                            ["if"] = "@function.inner",  -- Select inner part of a function
-                            ["ac"] = "@class.outer",     -- Select outer part of a class
-                            ["ic"] = "@class.inner",     -- Select inner part of a class
-                            ["aa"] = "@parameter.outer", -- Select outer part of a parameter
-                            ["ia"] = "@parameter.inner", -- Select inner part of a parameter
-                        },
-                    },
-                    -- Swap functionality
-                    swap = {
-                        enable = true,
-                        swap_next = {
-                            ["]p"] = "@parameter.inner", -- Swap parameter with next
-                            ["]f"] = "@function.outer",  -- Swap function with next
-                        },
-                        swap_previous = {
-                            ["[p"] = "@parameter.inner", -- Swap parameter with previous
-                            ["[f"] = "@function.outer",  -- Swap function with previous
-                        },
-                    },
-                    -- Movement between functions/classes
-                    move = {
-                        enable = true,
-                        set_jumps = true, -- Track in jumplist
-                        goto_next_start = {
-                            ["]m"] = "@function.outer",
-                            ["]]"] = "@class.outer",
-                        },
-                        goto_next_end = {
-                            ["]M"] = "@function.outer",
-                            ["]["] = "@class.outer",
-                        },
-                        goto_previous_start = {
-                            ["[m"] = "@function.outer",
-                            ["[["] = "@class.outer",
-                        },
-                        goto_previous_end = {
-                            ["[M"] = "@function.outer",
-                            ["[]"] = "@class.outer",
-                        },
-                    },
-                },
-            })
+				-- Syntax highlighting configuration
+				highlight = {
+					enable = true,
+					-- Disable vim regex highlighting for better performance
+					additional_vim_regex_highlighting = false,
+				},
 
-            -- Make the movements repeatable with ; and ,
-            local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
-            vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
-            vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
-        end,
-    },
+				-- Incremental selection: Expand selection based on syntax tree
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<C-space>", -- Start selection
+						node_incremental = "<C-space>", -- Expand selection to next node
+						scope_incremental = false, -- Disabled
+						node_decremental = "<bs>", -- Shrink selection (backspace)
+					},
+				},
+
+				-- Text objects: Operate on code structures (functions, classes, etc.)
+				textobjects = {
+					-- Select text objects
+					select = {
+						enable = true,
+						lookahead = true, -- Automatically jump forward to text object
+
+						keymaps = {
+							-- Function text objects
+							["af"] = "@function.outer", -- Select entire function including signature
+							["if"] = "@function.inner", -- Select function body only
+
+							-- Class text objects
+							["ac"] = "@class.outer", -- Select entire class including declaration
+							["ic"] = "@class.inner", -- Select class body only
+
+							-- Parameter/argument text objects
+							["aa"] = "@parameter.outer", -- Select parameter including commas
+							["ia"] = "@parameter.inner", -- Select parameter value only
+						},
+					},
+
+					-- Swap text objects (useful for reordering parameters, functions)
+					swap = {
+						enable = true,
+
+						-- Swap with next
+						swap_next = {
+							["]p"] = "@parameter.inner", -- Swap current parameter with next
+							["]f"] = "@function.outer", -- Swap current function with next
+						},
+
+						-- Swap with previous
+						swap_previous = {
+							["[p"] = "@parameter.inner", -- Swap current parameter with previous
+							["[f"] = "@function.outer", -- Swap current function with previous
+						},
+					},
+
+					-- Navigate between functions and classes
+					move = {
+						enable = true,
+						set_jumps = true, -- Add movements to jumplist (<C-o> to go back)
+
+						-- Go to next start
+						goto_next_start = {
+							["]m"] = "@function.outer", -- Next function start
+							["]]"] = "@class.outer", -- Next class start
+						},
+
+						-- Go to next end
+						goto_next_end = {
+							["]M"] = "@function.outer", -- Next function end
+							["]["] = "@class.outer", -- Next class end
+						},
+
+						-- Go to previous start
+						goto_previous_start = {
+							["[m"] = "@function.outer", -- Previous function start
+							["[["] = "@class.outer", -- Previous class start
+						},
+
+						-- Go to previous end
+						goto_previous_end = {
+							["[M"] = "@function.outer", -- Previous function end
+							["[]"] = "@class.outer", -- Previous class end
+						},
+					},
+				},
+			})
+
+			-- Make text object movements repeatable with ; (forward) and , (backward)
+			-- This allows you to press ]m once, then ; ; ; to keep moving forward
+			local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+			vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
+			vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
+		end,
+	},
 }
